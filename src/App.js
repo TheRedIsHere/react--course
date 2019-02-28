17c74519a7dd568c90c904184250303c8768f46c
Dzmitry Harbachou <mitya.gorbachev.2013@mail.ru>
2019-03-01 02:56:05 +0300

    dev/RC-4 Added new custom Component.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/*
+* Custom compoments
+*/
+import Person from './Person/Person';
+
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hello world</h1>
-    //   </div>
-    // );
+    return (
+      <div className="App">
+        <h1>Hello world</h1>
+        <Person/>
+      </div>
+    );
     
     // child elements are text elements
     //return React.createElement('div', null, 'h1', 'I\'m here');
 
     // child element is a HTMLElement
-    return React.createElement('div', {className: 'App'} , React.createElement('h1', null, 'Hello world!!!'));
+    //return React.createElement('div', {className: 'App'} , React.createElement('h1', null, 'Hello world!!!'));
   }
 }
 
