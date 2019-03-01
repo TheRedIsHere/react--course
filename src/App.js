ed17d48ada473cac8c2e5a20696564b2685fc8fc
Dzmitry Harbachou <mitya.gorbachev.2013@mail.ru>
2019-03-01 03:36:48 +0300

    dev/RC-8 Added states for work.

--- src/App.js
+++ src/App.js
@@ -8,13 +8,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Mark", age: "14"},
+      {name: "Dima", age: "21"},
+      {name: "Diana", age: "18"}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello world</h1>
-        <Person name="Max" age="14"/>
-        <Person name="Dima" age="21">My Hobbies: Racing</Person>
-        <Person name="Diana" age="18"/>
+        <button>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     
