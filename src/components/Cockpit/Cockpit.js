ebe7a2b9ededb5f223a611ab269507defd753f5a
Dzmitry Harbachou <mitya.gorbachev.2013@mail.ru>
2019-03-10 06:03:26 +0300

    DDCRI-7 Set up the useEffect function.

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -9,32 +9,38 @@ const cockpit = (props) => {
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
     // Http request...
-  });
+    setTimeout(() => {
+      alert('Saved data to cloud!');
+    }, 1000);
+    // also can set [] empty array as argument
+  }, [props.persons]);
 
-    const assignedClasses = [];
-    let btnClasses = '';
-     
-    if (props.showPerson) {
-        btnClasses = cssClasses.Red;
-    }
-    
-    if (props.persons.length <= 2) {
-      assignedClasses.push(cssClasses.red);
-    } 
-    
-    if (props.persons.length <= 1) {
-      assignedClasses.push(cssClasses.bold);
-    }
+  //useEffect();
 
-    return (
-        <div className={cssClasses.Cockpit}>
-            <h1>{props.tittle}</h1>
-            <p className={assignedClasses.join(' ')}>This is really working!</p>
-            <button 
-            className={btnClasses}
-            onClick={props.clicked}>Toggle Persons</button>
-        </div>
-    );
+  const assignedClasses = [];
+  let btnClasses = '';
+    
+  if (props.showPerson) {
+      btnClasses = cssClasses.Red;
+  }
+  
+  if (props.persons.length <= 2) {
+    assignedClasses.push(cssClasses.red);
+  } 
+  
+  if (props.persons.length <= 1) {
+    assignedClasses.push(cssClasses.bold);
+  }
+
+  return (
+      <div className={cssClasses.Cockpit}>
+          <h1>{props.tittle}</h1>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
+          <button 
+          className={btnClasses}
+          onClick={props.clicked}>Toggle Persons</button>
+      </div>
+  );
 };
 
 export default cockpit;
