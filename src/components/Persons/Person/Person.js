0c0b21aa144787c697b999bb092e0cf68398fdbc
Dzmitry Harbachou <mitya.gorbachev.2013@mail.ru>
2019-03-10 03:09:23 +0300

    DDCRI-4 Added updating state of the lifecycle methods.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 /*
 * Third party libraries
@@ -6,21 +6,25 @@ import React from 'react';
 
 import "./Person.css"
 
-const person = (props) => {
-    console.log('[Person.js] rendering...');
-    return (
-        <div className="Person">
-            <p onClick={props.click}>
-                I'm a {props.name} and I am {props.age}
-            </p>
-            <p>
-                {props.children}
-            </p>
-            <input type="text" 
-                onChange={props.changed}
-                value={props.name}/>
-        </div>
-    )
+class Person extends Component {
+
+    render() {
+        console.log('[Person.js] rendering...');
+
+        return (
+            <div className="Person">
+                <p onClick={this.props.click}>
+                    I'm a {this.props.name} and I am {this.props.age}
+                </p>
+                <p>
+                    {this.props.children}
+                </p>
+                <input type="text" 
+                    onChange={this.props.changed}
+                    value={this.props.name}/>
+            </div>
+        )
+    }
 }
 
-export default person;
+export default Person;
